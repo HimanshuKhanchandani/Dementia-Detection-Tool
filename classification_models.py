0f8ef532d4c76324d4bcaf679e4cd9acfbd87809
Himanshu Khanchandani <121183766+HimanshuKhanchandani@users.noreply.github.com>
2023-10-03 15:20:16 -0400

    Added different models and their performances
    I created a separate script called classification_models that contains different functions for different classification models. And their performances are tested in classification_performances notebook.

---
+++ classification_models.py
@@ -0,0 +1,179 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from processing_functions import create_numeric_labels, process_and_save
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+#from lightgbm import LGBMClassifier
+from sklearn.neural_network import MLPClassifier
+
+def remove_class(rbp_array,num_epochs,target_labels,class_):
+    if class_ == 'F':
+        return rbp_array[:65].copy(),num_epochs[:65].copy(),target_labels[:65].copy()
+    if class_ == 'A':
+        return rbp_array[36:].copy(),num_epochs[36:].copy(),target_labels[36:].copy()
+    if class_ == 'C':
+        return np.concatenate(rbp_array[:36],rbp_array[65:]), np.concatenate(num_epochs[:36],num_epochs[65:]), np.concatenate(target_labels[:36],target_labels[65:])
+
+def partial_flatten(rbp_array,num_epochs,target_labels,exclude=None,flatten_final=True):
+    
+    '''The sklearn models expect the input to take the form (number of examples) x (number of features). This requires flattening some of the dimensions of our arrays, which is done using the following helper function. The partial_flatten function reshapes the relative band power array so that the first dimension is epochs and the second dimension covers the relative band power for each of the five bands across all 19 channels. The num_epochs array we loaded in is passed as the second argument in order to easily exclude the zero-padded parts of the array. The function also returns a 1-d array of dimension (number of examples,) containing the corresponding class labels for each of these epochs. 
+
+The function assumes that classes that aren't used in the classification have already been removed from the rbp_array, i.e., if you are doing Alzheimer's/healthy classification (for instance) then all FTD examples have been removed. This can be done by feeding the rbp_array, num_epochs, and target_labels into the remove_class function first and then feeding the results into the partial_flatten function. 
+
+If the "exclude" argument is not None then the subject corresponding to that index is left out in the returned arrays. Note that this exclude index corresponds to the index of the subject in the rbp_array being fed into the function, which may not necessarily be the same as the subject's index in the original rbp array that we loaded. The flatten_final argument is a boolean that specifies whether or not the bands x channels part of the array should be flattened into a single dimension. This defaults to True, which is used for the sklearn models, but the non-flattened version will likely later be used in some other models (it's used in the CNN paper, for instance
+    
+    '''
+    total_subjects = len(target_labels)
+    feature_arrays = []
+    target_arrays = []
+    for i in range(total_subjects):
+        feature_arrays.append(rbp_array[i,0:num_epochs[i],:,:])
+        target_arrays.append(target_labels[i]*np.ones(num_epochs[i]))
+    if exclude==None: 
+        features= np.concatenate(feature_arrays)
+        targets = np.concatenate(target_arrays)
+    else:
+        features= np.concatenate(feature_arrays[:exclude] + feature_arrays[exclude+1:])
+        targets = np.concatenate(target_arrays[:exclude] + target_arrays[exclude+1:])
+    if flatten_final:
+        features = features.reshape((features.shape[0],-1))
+    return features, targets
+
+'''The next set of functions take in a 2 x 2 (total) confusion matrix presented as a numpy array and compute the metrics we are interested in. These functions assume that the 0-index of the confusion matrix corresponds to negative examples and the 1-index corresponds to positive examples. 
+
+In the comments TP = True Positive, TN = True Negative, FP = False Positive, FN = False Negative. 
+'''
+def accuracy(confusion):
+    # (TN + TP)/total
+    return (confusion[0,0]+confusion[1,1])/np.sum(confusion)
+def sensitivity(confusion):
+    # TP/(TP+FN)
+    return confusion[1,1]/(confusion[1,1]+confusion[1,0])
+def specificity(confusion):
+    # TN/(TN+FP)
+    return confusion[0,0]/(confusion[0,0]+confusion[0,1])
+def precision(confusion):
+    # TP/(TP+FP)
+    return confusion[1,1]/(confusion[1,1]+confusion[0,1])
+def f1(confusion):
+    # harmonic mean of precision and sensitivity
+    return 2*(precision(confusion)*sensitivity(confusion))/(precision(confusion)+sensitivity(confusion))
+
+def kNN_cross(rbp_array,num_epochs,target_labels,removed_class,n_neighbors):
+    '''
+    The function below runs our two-class kNN modeling routine with leave-one-subject-out cross-validation. It returns a dictionary of cross-validation accuracy, sensitivity, specificity, and F1 scores computed from the total confusion matrix. removed_class indicates the class to be excluded to create a two-class classification problem (same labels as the remove_class function) and n_neighbors indicates the number of neighbors k to use in kNN. 
+    '''
+    
+    if removed_class == 'F':
+        labels = [0,1]
+    if removed_class == 'A':
+        labels = [0,2]
+    if removed_class == 'C':
+        labels = [1,2]
+    confusion_matrices = []
+    mod_rbp, mod_num_epochs, mod_target_labels = remove_class(rbp_array,num_epochs,target_labels,removed_class)
+    for i in range(len(mod_target_labels)):
+        train_X, train_y = partial_flatten(mod_rbp,mod_num_epochs,mod_target_labels,exclude=i,flatten_final=True)
+        test_X = mod_rbp[i,0:mod_num_epochs[i],:,:].reshape(mod_num_epochs[i],-1)
+        test_y = mod_target_labels[i]*np.ones(mod_num_epochs[i])
+
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        
+        ThreeNN = KNeighborsClassifier(n_neighbors=n_neighbors)
+        ThreeNN.fit(train_X, train_y)
+        
+        test_X = scaler.transform(test_X)
+        
+        confusion_matrices += [confusion_matrix(test_y,ThreeNN.predict(test_X),labels=labels)]
+    confusion_matrices = np.array(confusion_matrices)
+    total_confusion = np.sum(confusion_matrices, axis= 0)
+    return {'acc':accuracy(total_confusion), 'sens':sensitivity(total_confusion), 'spec':specificity(total_confusion), 'f1':f1(total_confusion)}
+
+
+def RF_cross(rbp_array,num_epochs,target_labels,removed_class):
+    if removed_class == 'F':
+        labels = [0,1]
+    if removed_class == 'A':
+        labels = [0,2]
+    if removed_class == 'C':
+        labels = [1,2]
+    confusion_matrices = []
+    mod_rbp, mod_num_epochs, mod_target_labels = remove_class(rbp_array,num_epochs,target_labels,removed_class)
+    for i in range(len(mod_target_labels)):
+        train_X, train_y = partial_flatten(mod_rbp,mod_num_epochs,mod_target_labels,exclude=i,flatten_final=True)
+        test_X = mod_rbp[i,0:mod_num_epochs[i],:,:].reshape(mod_num_epochs[i],-1)
+        test_y = mod_target_labels[i]*np.ones(mod_num_epochs[i])
+
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        
+        RF = RandomForestClassifier()
+        RF.fit(train_X, train_y)
+        
+        test_X = scaler.transform(test_X)
+        
+        confusion_matrices += [confusion_matrix(test_y, RF.predict(test_X),labels=labels)]
+    confusion_matrices = np.array(confusion_matrices)
+    total_confusion = np.sum(confusion_matrices, axis= 0)
+    return {'acc':accuracy(total_confusion), 'sens':sensitivity(total_confusion), 
+            'spec':specificity(total_confusion), 'f1':f1(total_confusion)}
+
+def LightGBM_cross(rbp_array,num_epochs,target_labels,removed_class):
+    if removed_class == 'F':
+        labels = [0,1]
+    if removed_class == 'A':
+        labels = [0,2]
+    if removed_class == 'C':
+        labels = [1,2]
+    confusion_matrices = []
+    mod_rbp, mod_num_epochs, mod_target_labels = remove_class(rbp_array,num_epochs,target_labels,removed_class)
+    for i in range(len(mod_target_labels)):
+        train_X, train_y = partial_flatten(mod_rbp,mod_num_epochs,mod_target_labels,exclude=i,flatten_final=True)
+        test_X = mod_rbp[i,0:mod_num_epochs[i],:,:].reshape(mod_num_epochs[i],-1)
+        test_y = mod_target_labels[i]*np.ones(mod_num_epochs[i])
+
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        
+        LGBM = LGBMClassifier()
+        LGBM.fit(train_X, train_y)
+        
+        test_X = scaler.transform(test_X)
+        
+        confusion_matrices += [confusion_matrix(test_y, LGBM.predict(test_X),labels=labels)]
+    confusion_matrices = np.array(confusion_matrices)
+    total_confusion = np.sum(confusion_matrices, axis= 0)
+    return {'acc':accuracy(total_confusion), 'sens':sensitivity(total_confusion), 
+            'spec':specificity(total_confusion), 'f1':f1(total_confusion)}
+
+
+def MLP_cross(rbp_array,num_epochs,target_labels,removed_class, hidden_layer_sizes = (3,1)):
+    if removed_class == 'F':
+        labels = [0,1]
+    if removed_class == 'A':
+        labels = [0,2]
+    if removed_class == 'C':
+        labels = [1,2]
+    confusion_matrices = []
+    mod_rbp, mod_num_epochs, mod_target_labels = remove_class(rbp_array,num_epochs,target_labels,removed_class)
+    for i in range(len(mod_target_labels)):
+        train_X, train_y = partial_flatten(mod_rbp,mod_num_epochs,mod_target_labels,exclude=i,flatten_final=True)
+        test_X = mod_rbp[i,0:mod_num_epochs[i],:,:].reshape(mod_num_epochs[i],-1)
+        test_y = mod_target_labels[i]*np.ones(mod_num_epochs[i])
+
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        
+        MLP = MLPClassifier(hidden_layer_sizes = hidden_layer_sizes)
+        MLP.fit(train_X, train_y)
+        
+        test_X = scaler.transform(test_X)
+        
+        confusion_matrices += [confusion_matrix(test_y, MLP.predict(test_X),labels=labels)]
+    confusion_matrices = np.array(confusion_matrices)
+    total_confusion = np.sum(confusion_matrices, axis= 0)
+    return {'acc':accuracy(total_confusion), 'sens':sensitivity(total_confusion), 
+            'spec':specificity(total_confusion), 'f1':f1(total_confusion)}
--- processing_functions.py
+++ processing_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.integrate import simpson
+import mne
+from scipy.signal import welch
 
 def ppt_id(i):
     return 'sub-' + str(i).zfill(3)
@@ -43,3 +45,51 @@ def create_numeric_labels(group_name):
         return 2
     if group_name == 'C':
         return 0
+        
+def process(epoch_length,overlap_ratio,freq_bands,nperseg, absolute=False, sample_freq=500,total_subjects=88):
+    features = []
+    if absolute:
+        absolute_features = []
+        total_features = []
+    for i in range(total_subjects):
+        ppt = i + 1
+        raw_data = mne.io.read_raw_eeglab('data/ds004504/derivatives/' + ppt_id(ppt)
+                                    + '/eeg/' + ppt_id(ppt) + '_task-eyesclosed_eeg.set', preload = True)
+        export = raw_data.to_data_frame()
+        ppt_array = export.iloc[:,range(1,len(export.columns))].values
+        ppt_epochs = create_epochs(ppt_array,epoch_length,overlap_ratio)
+        freqs, ppt_psd  = welch(ppt_epochs,fs=sample_freq,nperseg=nperseg, axis=1)
+        ppt_rbp = relative_band_power(ppt_psd,freqs,freq_bands)
+        features += [ppt_rbp]
+        if absolute:
+            ppt_abp = absolute_band_power(ppt_psd,freqs,freq_bands)
+            ppt_tbp = absolute_band_power(ppt_psd,freqs,[freq_bands[0],freq_bands[-1]])
+            absolute_features += [ppt_abp]
+            total_features += [ppt_tbp]
+    num_epochs = np.array([feature.shape[0] for feature in features])
+    max_epoch_length = np.max(num_epochs)
+    for i in range(total_subjects):
+        shape = list(features[i].shape)
+        shape[0] = max_epoch_length-shape[0]
+        features[i] = np.concatenate((features[i],np.zeros(shape)),axis=0)
+        if absolute:
+            absolute_features[i] = np.concatenate((absolute_features[i],np.zeros(shape)),axis=0)
+            total_shape = list(total_features[i].shape)
+            total_shape[0] = max_epoch_length-total_shape[0]
+            total_features[i] = np.concatenate((total_features[i],np.zeros(total_shape)),axis=0)
+    if not absolute:
+        return num_epochs, np.array(features)
+    else:
+        return num_epochs, np.array(features), np.array(absolute_features), np.array(total_features)
+    
+def process_and_save(epoch_length,overlap_ratio,freq_bands,nperseg, filenames, absolute=False, sample_freq=500,total_subjects=88):
+    if not absolute:
+        num_epochs, rbp_features = process(epoch_length,overlap_ratio,freq_bands,nperseg, absolute=absolute, sample_freq=sample_freq,total_subjects=total_subjects)
+        np.save(filenames[0],num_epochs)
+        np.save(filenames[1],rbp_features)
+    else:
+        num_epochs, rbp_features, abp_features, tbp_features = process(epoch_length,overlap_ratio,freq_bands,nperseg, absolute=absolute, sample_freq=sample_freq,total_subjects=total_subjects)
+        np.save(filenames[0],num_epochs)
+        np.save(filenames[1],rbp_features)
+        np.save(filenames[2],abp_features)
+        np.save(filenames[3],tbp_features)
