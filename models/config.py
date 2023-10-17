4b47a18078f1f3b3e96d4be75d6fa4abcd934353
conformalbp <132406003+conformalbp@users.noreply.github.com>
2023-10-17 15:47:37 -0400

    added data loading

--- models/config.py
+++ models/config.py
@@ -1,2 +1,4 @@
-#file path to the data folder
-PATH = '../data/ds004504'
+#filepath to the data folder
+DATA_PATH = '../data/ds004504'
+#filepath to the folder storing data processed with mne
+PROCESSED_DATA_PATH = '../processed_data'
