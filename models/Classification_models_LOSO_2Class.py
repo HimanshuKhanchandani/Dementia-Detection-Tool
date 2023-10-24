e968fcd2c067b06774dd2c0c34808a9e4a7f5104
conformalbp <132406003+conformalbp@users.noreply.github.com>
2023-10-23 23:26:01 -0400

    added mlp classifier for cross-validation

--- models/Classification_models_LOSO_2Class.py
+++ models/Classification_models_LOSO_2Class.py
@@ -6,6 +6,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.decomposition import KernelPCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.decomposition import PCA
+from sklearn.neural_network import MLPClassifier
 #import xgboost as xgb
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
@@ -276,6 +277,73 @@ def svm_cross(rbps,targets,kernel,reg_parameter=1.0, degree=2, PCA_components = 
                             'spec':specificity(total_confusion_test), 'f1':f1(total_confusion_test)}
 
 
+    return train_metrics_dict, test_metrics_dict
+
+def mlp_cross(rbps,targets, PCA_components = 0, alpha = 0.0001, learning_rate_init = 0.001, hidden_layer_sizes = (3,1), max_iter = 200):
+    
+    '''
+    Performs training on features in rbps and targets using multi-layer perceptron classification and performs cross-validation 
+    using leave one subject out method. 
+    
+    Parameters
+    ----------
+    rbps : list[ndarray]
+        List of feature arrays corresponding to each subject. 
+    targets : ndarray
+        Array of numeric class labels for each feature array.
+    PCA_components: int
+        Number of components to keep for principal components analysis. Defaults to 0 which is the case when we don't do PCA.
+    alpha: float
+        Parameter describing strength of L2 regularization term. 
+    max_iter: int
+        Maximum number of iterations for solver. 
+    learning_rate_init: float
+        Initial learning rate used.
+    hidden_layer_sizes: 
+        tuple describing the number of neurons in each hidden layer.
+           
+    Returns
+    -------
+    train_metrics_dict : dictionary
+        Dictionary containing accuracy, sensitivity, specificity and f1 scores on the training data.
+    test_metrics_dict : dictionary
+        Dictionary containing accuracy, sensitivity, specificity and f1 scores on the validation  data.
+    
+    '''
+    confusion_matrices_train = []
+    confusion_matrices_test = []
+    labels = np.unique(targets)
+    for i in range(len(targets)):
+        train_X, train_y = train_prep(rbps,targets,exclude=i,flatten_final=True)
+        test_X = rbps[i].reshape(rbps[i].shape[0],-1)
+        test_y = targets[i]*np.ones(rbps[i].shape[0])
+
+        scaler = StandardScaler()
+        train_X = scaler.fit_transform(train_X)
+        test_X = scaler.transform(test_X)
+        
+        if PCA_components != 0:
+            pca = PCA(n_components = PCA_components)
+            train_X = pca.fit_transform(train_X, y = None)
+            test_X = pca.transform(test_X)
+        
+        mlp = MLPClassifier(hidden_layer_sizes = hidden_layer_sizes, max_iter = max_iter,alpha = alpha, learning_rate_init = learning_rate_init)
+        mlp.fit(train_X, train_y)
+        
+        confusion_matrices_train += [confusion_matrix(train_y, mlp.predict(train_X),labels=labels)]
+        confusion_matrices_test += [confusion_matrix(test_y, mlp.predict(test_X),labels=labels)]
+    
+    confusion_matrices_train = np.array(confusion_matrices_train)
+    confusion_matrices_test = np.array(confusion_matrices_test)
+    total_confusion_train = np.sum(confusion_matrices_train, axis= 0)
+    total_confusion_test = np.sum(confusion_matrices_test, axis= 0)
+    
+    train_metrics_dict = {'acc':accuracy(total_confusion_train), 'sens':sensitivity(total_confusion_train), 
+                            'spec':specificity(total_confusion_train), 'f1':f1(total_confusion_train)}
+    test_metrics_dict = {'acc':accuracy(total_confusion_test), 'sens':sensitivity(total_confusion_test), 
+                            'spec':specificity(total_confusion_test), 'f1':f1(total_confusion_test)}
+    
+    
     return train_metrics_dict, test_metrics_dict
     
     
